760fdb772f6edd0cd9300a2482082e60670b89fc
Edson Vieira da Silva <edson.2012@alunos.utfpr.edu.br>
2023-10-31 15:02:04 -0300

    cabeçalho e rodapé com materialize

--- src/app/componente2-cabecalho/cabecalho/cabecalho.component.ts
+++ src/app/componente2-cabecalho/cabecalho/cabecalho.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as M from 'materialize-css';
+//declare var M: any;
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
   styleUrls: ['./cabecalho.component.css']
 })
-export class CabecalhoComponent {
+export class CabecalhoComponent implements OnInit {
+
+ngOnInit(): void {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems,{});
+}
+
 
 }
