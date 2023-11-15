2367ce264d85ed60ce3ecc9b6c2e571d4ed5ed81
Edson Vieira da Silva <edson.2012@alunos.utfpr.edu.br>
2023-11-15 11:17:13 -0300

    aaa

--- src/app/tela-cadastro/tela-cadastro.component.ts
+++ src/app/tela-cadastro/tela-cadastro.component.ts
@@ -1,24 +1,31 @@
 import { Router } from '@angular/router';
 import { Usuario } from './../Model/usuario';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-tela-cadastro',
   templateUrl: './tela-cadastro.component.html',
   styleUrls: ['./tela-cadastro.component.css']
 })
-export class TelaCadastroComponent implements OnInit{
+export class TelaCadastroComponent implements OnInit, AfterViewInit{
 
     usuario!: Usuario
 
     constructor(private router: Router){}
 
     ngOnInit(): void {
+
       var elems = document.querySelectorAll('select');
       var instances = M.FormSelect.init(elems);
       
-      this.usuario = new Usuario('','',0, 0, 0,1.2);
-  
+      this.usuario = new Usuario('','',null, null, null,1.2);
+      
+      
+    }
+
+    ngAfterViewInit(): void {
+      //
     }
 
     //envia id do usuario via rota e armazena o web storage
